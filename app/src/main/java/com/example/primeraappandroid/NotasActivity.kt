9e91568457b2b1646d46ae151038df85560b9eca
Sala E5 <CSalaE5@uniboyaca.edu.co>
2024-10-25 19:06:05 -0500

    Se agregaron las condiciones de la nota para saber si aprobo o no

--- app/src/main/java/com/example/primeraappandroid/NotasActivity.kt
+++ app/src/main/java/com/example/primeraappandroid/NotasActivity.kt
@@ -16,6 +16,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
@@ -37,30 +41,69 @@ class NotasActivity : ComponentActivity() {
 
 @Composable
 fun NotasPage() {
+    var actividad by remember { mutableStateOf<String>(value = "") }
+    var nota by remember { mutableStateOf<Double>(value = 0.0) }
+
     PrimeraAppAndroidTheme {
-        Column(
-            modifier = Modifier.padding(12.dp)
-        ) {
-            Row(
-                modifier = Modifier.padding(bottom = 12.dp)
-            ) {
-                Text(text = stringResource(id = R.string.actividad_text))
-                TextField(value = "", onValueChange = {})
-            }
-            Row(
-                modifier = Modifier.padding(bottom = 12.dp)
-            ) {
-                Text(text = stringResource(id = R.string.nota_text))
-                TextField(value = "", onValueChange = {})
-            }
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = colorResource(id = R.color.purple_200)),
-                onClick = {}) {
-                Text(stringResource(id = R.string.guardar_btn))
-            }
+        Column {
+            NotasForm(
+                actividad = actividad,
+                nota = nota,
+                setActividad = {actividad=it},
+                setNota = {nota=it},
+
+                )
+            ValidacionNota(
+                actividad = actividad,
+                nota = nota
+            )
         }
+    }
+}
+
+@Composable
+fun NotasForm(
+    actividad: String,
+    nota: Double,
+    setActividad: (value:String)->Unit,
+    setNota: (value:Double)->Unit
+) {
+    Column(
+        modifier = Modifier.padding(12.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(bottom = 12.dp)
+        ) {
+            Text(text = stringResource(id = R.string.actividad_text))
+            TextField(value = actividad,
+                onValueChange = { value -> setActividad(value) }
+                    )
+        }
+        Row(
+            modifier = Modifier.padding(bottom = 12.dp)
+        ) {
+            Text(text = stringResource(id = R.string.nota_text))
+            TextField(value = nota.toString(), onValueChange = {setNota(it.toDouble())})
+        }
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = colorResource(id = R.color.purple_200)),
+            onClick = {
+                setActividad("54321")
+            }) {
+            Text(stringResource(id = R.string.guardar_btn))
+        }
+    }
+}
+
+
+@Composable
+fun ValidacionNota(actividad: String, nota: Double) {
+    if (nota >= 3) {
+        Text(text = "Aprobó la actividad de: " + actividad)
+    } else {
+        Text(text = "Reprobó la actividad de: " + actividad)
     }
 }
 
